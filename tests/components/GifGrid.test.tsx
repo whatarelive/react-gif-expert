06b325f669c021674cc7eaaad61a0bc9353f0880
whatarelive <128400552+whatarelive@users.noreply.github.com>
2024-04-09 23:32:33 +0200

    9no Commit

--- tests/components/GifGrid.test.tsx
+++ tests/components/GifGrid.test.tsx
@@ -1,18 +1,79 @@
+/* eslint-disable no-undef */
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/Components";
+import { useFecthGifs } from "../../src/hooks/useFecthGifs";
 
+// Creamos un mock personalizado del customHook.
+jest.mock("../../src/hooks/useFecthGifs");
 
 describe('Pruebas en el componente <GifGrid/>', () => {
 
-    // Test #1: Sobre la UI del Componente.
-    test('Snapshot del Componente',() => {
+    // Variable auxiliar
+    const category = "One Punch";
 
-        // Se guarda el contenido del componente renderizado.
-        const { container } = render(<GifGrid category="One Punch"/>);
+    // Test #1: Sobre la UI inicial del componente
+    test('Snapshot del componente', () => { 
 
-        // Se crea una captura del elemento renderizado.
+        // Definimos el valor de retorno de nuestro CustomHook
+        (useFecthGifs as jest.Mock).mockReturnValue({
+            images: [],
+            isLoading: true
+        })
+
+        // Renderizamos el componente y guardamos el render.
+        const { container } = render( <GifGrid category={ category }/>);
+
+        // Creamos una captura del componente.
         expect( container ).toMatchSnapshot();
-    });
+    })
+    
+    
+    // Test #2: Sobre el mensaje de Carga.
+    test('debe mostrar el loading inicialmente', () => {
+        
+        // Definimos el valor de retorno de nuestro CustomHook
+        (useFecthGifs as jest.Mock).mockReturnValue({
+            images: [],
+            isLoading: true
+        });
 
+        // Renderizamos el componente
+        render( <GifGrid category={ category }/>);
+
+        // Esperamos que se muestre un elemento con el texto especificado.
+        expect( screen.getByText('Cargando.....') );
+        expect( screen.getByText( category ) );
+    })
+
+
+    // Test #3: Sobre la visibilidad de la lista de gifs.
+    test('debe mostrar items cuando se cargan las imagenes useFetchGifs', () => { 
+
+        // Creamos un arreglo auxiliar de elementos a mostrar.
+        const gifs = [
+            {
+                id: 'ABC',
+                title: 'Saitama',
+                url: 'https://localhost/saitama.jpg'
+            },
+            {
+                id: '123',
+                title: 'Goku',
+                url: 'https://localhost/goku.jpg'    
+            }
+        ];
+
+        // Definimos el valor de retorno de nuestro CustomHook
+        (useFecthGifs as jest.Mock).mockReturnValue({
+            images: gifs,
+            isLoading: false
+        });
+        
+        // Renderizamos el componente.
+        render( <GifGrid category={ category }/>);
+        
+        // Esoeramos que halla dos elementos de tipo imagen en el HTML.
+        expect( screen.getAllByRole('img').length ).toBe(2);
+    })
 })
